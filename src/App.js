88c5ebd67d9d39c40282d56b64fb7d59a516b84d
ibrahim_glmz <gulhezibrahim@gmail.com>
2024-12-28 18:23:16 +0300

    paket yüklendi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Quiz from './components/Quiz';
 import './App.css';
 
 function App() {
+  const sureAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/sounds/sureSes.mp3`));
+  const bombaAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/sounds/bombaSes.mp3`));
+
   return (
     <div className="app">
       <Quiz />
