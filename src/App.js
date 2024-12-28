df535dffc9d1f9e149fef82f2269ce90ae24deca
ibrahim_glmz <gulhezibrahim@gmail.com>
2024-12-28 18:46:36 +0300

    Fix deployment configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useRef } from 'react';
 import Quiz from './components/Quiz';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   const sureAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/assets/sureSes.mp3`));
   const bombaAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/assets/bombaSes.mp3`));
 
   return (
-    <div className="app">
+    <Router basename="/quiz-app">
       <Quiz />
-    </div>
+    </Router>
   );
 }
 
