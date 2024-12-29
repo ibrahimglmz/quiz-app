62f164235d996cd4501b106f34c90b6abe994c92
ibrahim_glmz <gulhezibrahim@gmail.com>
2024-12-29 16:31:42 +0300

    Update game title to Kütle Ağırlık Kaçağı

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Quiz from './components/Quiz';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  const sureAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/assets/sureSes.mp3`));
-  const bombaAudioRef = useRef(new Audio(`${process.env.PUBLIC_URL}/assets/bombaSes.mp3`));
-
   return (
-    <Router basename="/quiz-app">
+    <div className="App">
       <Quiz />
-    </Router>
+    </div>
   );
 }
 
